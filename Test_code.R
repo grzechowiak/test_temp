d8c057cf1e67d5d6d3d003c13f6b677550527edc
Marcin Grzechowiak <grzechowiak@users.noreply.github.com>
2019-07-17 22:31:32 -0500

    Update Test_code.R

--- Test_code.R
+++ Test_code.R
@@ -1,4 +1,4 @@
-# Create a function
+### Create a function
 jobOffers <- function(scores, lowerLimits, upperLimits){
   
   #Define matrix[1, length(lowerLimits)]
@@ -10,21 +10,23 @@ jobOffers <- function(scores, lowerLimits, upperLimits){
     for(i in scores) {
       #check if the scores is between given range
       y <- append(y,ifelse(i >= lowerLimits[j] & i <= upperLimits[j], print(1), print(0)))
-      #transform to numeric and sum up all cases when we it is 1 (1 means someone is in the range, so we count it)
+      #transform to numeric and sum up all cases when equal to 1 (1 means someone is in the range, so we count it)
       y<- as.numeric(y)
       y <- sum(y)
     }
+    # Store result in the final matrix
     x[j] <- y
   }
   return(x)
 }
 
-# Example data (given)
+
+### Example data (given)
 scores <- c(2,3,8,3,7,4,2,10,100)
 lowerLimits <- c(1,3)
 upperLimits <- c(4,11)
 
 
-# Call the function and show the matrix
+### Call the function and show the matrix
 answer <- jobOffers(scores, lowerLimits, upperLimits)
 answer
